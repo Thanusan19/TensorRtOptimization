29ecc1e534e0761caa1a5fd4337f445deb426b2a
localadmin <stefchr.cy@gmail.com>
2020-02-13 18:11:10 +0100

    Launch TensorRT engine and load our uff model

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TensorRtOptimization
-Special Problem Georgia Tech. Optimize Deep Learning Model by unsing TensorRT and cuda 
+Special Problem Georgia Tech. Optimize Deep Learning Model by unsing TensorRT and cuda.
+
+
+## Install required libraries
+All needed libraries are in the 'requirements.txt' file.
+To install run the following command on a terminal:
+-  in python3:`python3 -m pip install -r requirements.txt`
+-  in python2:`python2 -m pip install -r requirements.txt`
+
+
+
--- modelGetGraphDef.py
+++ modelGetGraphDef.py
@@ -8,6 +8,7 @@ from tensorflow.summary import FileWriter
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
 import uff
+#import pycuda.driver as cuda
 #from tensorrt.parsers import uffparser
 
 #import pycuda.driver as cuda
@@ -19,6 +20,16 @@ from matplotlib.pyplot import imshow #to show test case
 import time #import system tools
 import os
 #-----------------------------------------------------
+import numpy as np
+
+import pycuda.driver as cuda
+import pycuda.autoinit
+import tensorrt as trt
+import sys
+#sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import common
+#from common import * 
+#----------------------------------------------------
 
 
 ROOT = '/home/localadmin/Documents/MLP/model_ckpt'
@@ -26,86 +37,112 @@ PATH = ROOT+'/Best_1S'
 X = np.load('/home/localadmin/Documents/TEST_SET_S2S_X.npy')
 Y = np.load('/home/localadmin/Documents/TEST_SET_S2S_Y.npy')
 
-sess = tf.InteractiveSession()
+# You can set the logger severity higher to suppress messages (or lower to display more messages).
+TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 
-saver = tf.train.import_meta_graph(PATH+'.meta')
-saver.restore(sess, PATH)
+class ModelData(object):
+    MODEL_FILE = "model.uff"
+    INPUT_NAME ="inputs"
+    INPUT_SHAPE = X.shape[1:]
+    OUTPUT_NAME = "output/BiasAdd"
 
-PRED = sess.run('output/BiasAdd:0',feed_dict={'inputs:0':X})#,'hidden_state:0':np.zeros((3,X.shape[0],64))})
+def GiB(val):
+    return val * 1 << 30
 
-# RNN, GRU OK
-HS = np.zeros((3,X.shape[0],64))
-# LSTM 
-#HS = np.zeros((3,2,X.shape[0],64))
-# MLP, CNN
-# No HS, input --> inputs, pred [:,-1,:] --> [:,:]
+def build_engine(model_file):
+    # For more information on TRT basics, refer to the introductory samples.
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_network() as network, trt.UffParser() as parser:
+        builder.max_workspace_size =GiB(1)
+        # Parse the Uff Network
+        parser.register_input(ModelData.INPUT_NAME, ModelData.INPUT_SHAPE)
+        parser.register_output(ModelData.OUTPUT_NAME)
+        parser.parse(model_file, network)
+        # Build and return an engine.
+        return builder.build_cuda_engine(network)
 
-
-start = datetime.datetime.now()
-for i in range(10):
-    PRED = sess.run('output/BiasAdd:0',feed_dict={'inputs:0':X})#,'hidden_state:0':HS})
-end = datetime.datetime.now()
-
-elapsed_time = (end-start).total_seconds()
-#RMSE = np.sqrt(np.mean((PRED[:,-1,:] - Y[:,-1,:])**2))
-#STD = np.std((PRED[:,-1,:] - Y[:,-1,:])**2)
-RMSE = np.sqrt(np.mean((PRED[:,:] - Y[:,-1,:])**2))
-STD = np.std((PRED[:,:] - Y[:,-1,:])**2)
-
-print("Elapsed time without TensorRT: ")
-print(elapsed_time)
-
-print(Y[:,-1,:])
-
-FileWriter("__tb", sess.graph)
-#--------------------------------------------------#
-#Get output nodes  Names
-#--------------------------------------------------#
-graph = sess.graph
-#print([node.name for node in graph.as_graph_def().node])
-output_node_names=[node.name for node in graph.as_graph_def().node]
-
-#----------------------------------------------------------------#
-#Make a frozen model(.pb) of the TF  model in order to convert it into UFF#
-#----------------------------------------------------------------#
-# We use a built-in TF helper to export variables to constants
-output_graph_def = tf.graph_util.convert_variables_to_constants(
-           sess, # The session is used to retrieve the weights
-           tf.get_default_graph().as_graph_def(), # The graph_def is used to retrieve the nodes 
-           output_node_names # The output node names are used to select the usefull nodes
-        ) 
-
-input_checkpoint=PATH
-# We precise the file fullname of our freezed graph
-absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
-output_graph = absolute_model_dir + "/frozen_model.pb"
-
-# Finally we serialize and dump the output graph to the filesystem
-with tf.gfile.GFile(output_graph, "wb") as f:
-    f.write(output_graph_def.SerializeToString())
-print("%d ops in the final graph." % len(output_graph_def.node))
-
-#----------------------------------#
-#Conversion TF graph def as UFF #
-#---------------------------------#
-
-#with tf.Session() as sess:
-    # First deserialize your frozen graph:
-#    with tf.gfile.GFile('/home/localadmin/Documents/GRU/model_ckpt/frozen_model.pb', 'rb') as f:
-#        frozen_graph = tf.GraphDef()
- #       frozen_graph.ParseFromString(f.read())
-    # Now you can create a TensorRT inference graph from your
-    # frozen graph:
-  #  converter = trt.TrtGraphConverter(
-  #          input_graph_def=frozen_graph,
-  #          nodes_blacklist=['logits', 'classes']) #output nodes
-  #  trt_graph = converter.convert()
-    # Import the TensorRT graph into a new graph and run:
-    #output_node = tf.import_graph_def(
-    #    trt_graph,
-    #    return_elements=['logits', 'classes'])
-    #sess.run(output_node)           
+        
+def do_inference(context, bindings, inputs, outputs, stream, batch_size=1):
+    # Transfer input data to the GPU.
+    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
+    # Run inference.
+    context.execute_async(batch_size=batch_size, bindings=bindings, stream_handle=stream.handle)
+    # Transfer predictions back from the GPU.
+    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
+    # Synchronize the stream
+    stream.synchronize()
+    # Return only the host outputs.
+    return [out.host for out in outputs]
 
 
-uff_model = uff.from_tensorflow_frozen_model(ROOT+'/frozen_model.pb',['output/BiasAdd'],output_filename = 'model.uff')
+def main():
+	sess = tf.InteractiveSession()
 
+	saver = tf.train.import_meta_graph(PATH+'.meta')
+	saver.restore(sess, PATH)
+
+	PRED = sess.run('output/BiasAdd:0',feed_dict={'inputs:0':X})#,'hidden_state:0':np.zeros((3,X.shape[0],64))})
+
+	# RNN, GRU OK
+	HS = np.zeros((3,X.shape[0],64))
+	# LSTM 
+	#HS = np.zeros((3,2,X.shape[0],64))
+	# MLP, CNN
+	# No HS, input --> inputs, pred [:,-1,:] --> [:,:]
+
+
+	start = datetime.datetime.now()
+	for i in range(10):
+	    PRED = sess.run('output/BiasAdd:0',feed_dict={'inputs:0':X})#,'hidden_state:0':HS})
+	end = datetime.datetime.now()
+
+	elapsed_time = (end-start).total_seconds()
+	#RMSE = np.sqrt(np.mean((PRED[:,-1,:] - Y[:,-1,:])**2))
+	#STD = np.std((PRED[:,-1,:] - Y[:,-1,:])**2)
+	RMSE = np.sqrt(np.mean((PRED[:,:] - Y[:,-1,:])**2))
+	STD = np.std((PRED[:,:] - Y[:,-1,:])**2)
+
+	print("Elapsed time without TensorRT: ")
+	print(elapsed_time)
+
+	print(Y[:,-1,:])
+
+	FileWriter("__tb", sess.graph)
+	#--------------------------------------------------#
+	#Get output nodes  Names
+	#--------------------------------------------------#
+	graph = sess.graph
+	#print([node.name for node in graph.as_graph_def().node])
+	output_node_names=[node.name for node in graph.as_graph_def().node]
+
+	#----------------------------------------------------------------#
+	#Make a frozen model(.pb) of the TF  model in order to convert it into UFF#
+	#----------------------------------------------------------------#
+	# We use a built-in TF helper to export variables to constants
+	output_graph_def = tf.graph_util.convert_variables_to_constants(
+		   sess, # The session is used to retrieve the weights
+		   tf.get_default_graph().as_graph_def(), # The graph_def is used to retrieve the nodes 
+		   output_node_names # The output node names are used to select the usefull nodes
+		) 
+
+	input_checkpoint=PATH
+	# We precise the file fullname of our freezed graph
+	absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
+	output_graph = absolute_model_dir + "/frozen_model.pb"
+
+	# Finally we serialize and dump the output graph to the filesystem
+	with tf.gfile.GFile(output_graph, "wb") as f:
+	    f.write(output_graph_def.SerializeToString())
+	print("%d ops in the final graph." % len(output_graph_def.node))
+
+	#----------------------------------#
+	#Conversion TF graph def as UFF #
+	#---------------------------------#
+	uff_model = uff.from_tensorflow_frozen_model(ROOT+'/frozen_model.pb',['output/BiasAdd'],output_filename = 'model.uff')
+
+
+	#G_LOGGER = trt.infer.ConsoleLogger(trt.infer.LogSeverity.ERROR)
+	model_file='model.uff'
+	builder=build_engine(model_file)
+	
+if __name__ == '__main__':
+    main()
